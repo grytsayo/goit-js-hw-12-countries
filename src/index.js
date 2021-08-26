c119ba3d8a144d4973598c0b72d5c7165d3debd9
Oleg Grytsay <grytsayo@gmail.com>
2021-08-26 22:06:17 +0300

    bugFix and markUp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 import './sass/main.scss';
 import './js/fetchCountries';
-
-import { error } from '../node_modules/@pnotify/core/dist/PNotify.js';
-
-error({
-    text: 'Too many matches found!'
-      });
